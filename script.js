5742c50d35fd6d3faaf1be99546d3dd230186f8c
rocket3989 <rocket3989@gmail.com>
2019-10-16 08:46:33 -0500

    maybe done

--- script.js
+++ script.js
@@ -33,6 +33,11 @@ function changeColor(){
     reDraw();
 }
 
+function changeSeed(){
+	colorChoice ^= 2;
+	reDraw();
+}
+
 function reDraw(){
     byGroup.forEach(drawGroup)
 }
@@ -70,16 +75,38 @@ function scoreMatches(matches){
     })
 
 
-    var advanceSlots = 2
+	var advanceSlots = 2
+	
     d3.nestBy(teams, function(d){ return d.w + d.wins*10 })
         .sort(d3.descendingKey('key'))
         .forEach(function(d){
         if (d.length <= advanceSlots){
-            d.forEach(function(d){ d.advance = 't'})
+			d.forEach(function(d){ d.advance = 't'})
+			if(advanceSlots == 2){
+				if(d.length == 2){ //check for tied first place
+					against = matches.filter(match => {return match.allTeams == d[0].name + '-' + d[1].name 
+														   || match.allTeams == d[1].name + '-' + d[0].name})
+					if(against[0].actualWinner == against[1].actualWinner){
+						d.forEach(function(d){ if (d.name == against[0].wName) d.advance = 'u'})
+					}
+				}
+				else
+					d.forEach(function(d){ d.advance = 'u'})
+			}
         } else if (advanceSlots > 0){
             d.forEach(function(d){ d.advance = 'm'})
         } else{
-            d.forEach(function(d){ d.advance = 'f' })
+			d.forEach(function(d){ d.advance = 'f' })
+			if(d.length == 2){ //check for tied last place
+				against = matches.filter(match => {return match.allTeams == d[0].name + '-' + d[1].name 
+													   || match.allTeams == d[1].name + '-' + d[0].name})
+				if(against[0].actualWinner == against[1].actualWinner){
+					d.forEach(function(d){ if (d.name == against[0].wName) d.advance = 'e'})
+				}
+			}
+			if(advanceSlots == - 1){
+				d[0].advance = 'e'
+			}
         }
         advanceSlots -= d.length
         })
@@ -221,10 +248,10 @@ function drawResults(sel, scenarios, name, complete, incomplete){
         if (d.key == '110') s = against[2] 
         return s
         })
-        .at({textAnchor: 'middle', x: 10*3.5, y: -10})
-
+		.at({textAnchor: 'middle', x: 10*3.5, y: -10})
+	
     recordSel.appendMany('circle.scenario', ƒ())
-        .at({r: 5, fill: ƒ('team', color[colorChoice]), cx: function(d, i){ return i*10} })
+        .at({r: 5, fill: ƒ('team', color[colorChoice]), cx: function(d, i){return i*10} })
         .call(d3.attachTooltip)
         .on('mouseout', function(){gSel.selectAll('circle.scenario').classed('active', false).at('r', 5) })
         .on('mouseover', function(d){
@@ -244,12 +271,20 @@ function drawResults(sel, scenarios, name, complete, incomplete){
 
         var byAdvanceSel = tt.appendMany('div.advance', d3.nestBy(d.teams, ƒ('advance')).sort(d3.descendingKey('key')))
             .text(function(d){
-            return d.map(ƒ('name')).join(' and ') + {t: ' advance', m: ' tie', f: (d.length > 1 ? ' are' : ' is') + ' eliminated'}[d.key]
+            return d.map(ƒ('name')).join(' and ') + {u:' first seed',t: ' advance', m: ' tie', f: (d.length > 1 ? ' are' : ' is') + ' eliminated', e:' last place'}[d.key]
             })
         })
+		.on('click', function(d){
+			d3.selectAll('.matches > .game').each(function(e, i){
+				if (e.group == d.incomplete[0].group){
+					e.clicked = parseInt(d.str[i % 6]) - 1
+					d3.select(this).dispatch("click")
+				}
+			})
+		})
 
     var swoopy = d3.swoopyDrag()
-        .draggable(1)
+        .draggable(0)
         .x(function(){ return 0 })
         .y(function(){ return 0 })
         .annotations(annotations.filter(function(d){ return d.team == name }))
@@ -266,8 +301,10 @@ function drawResults(sel, scenarios, name, complete, incomplete){
 }
 
 
-color = [ function color(d){ return {t: '#4CAF50', m: '#FF9800', f: '#F44336'}[d.advance] },
-          function color(d){ return {t: '#2c7bb6', m: '#dfaf90', f: '#d7191c'}[d.advance] }]
+color = [ function color(d){ return {u:'#4CAF50',t: '#4CAF50', m: '#FF9800', f: '#f73434', e: '#f73434'}[d.advance] },
+		  function color(d){ return {u:'#2c7bb6',t: '#2c7bb6', m: '#dfaf90', f: '#f73434', e: '#f73434'}[d.advance] },
+	 	  function color(d){ return {u:'#36e03d',t: '#4CAF50', m: '#FF9800', f: '#f73434', e: '#b80f02'}[d.advance] },
+          function color(d){ return {u:'#5aa4db',t: '#2c7bb6', m: '#dfaf90', f: '#f73434', e: '#b80f02'}[d.advance] }]
 
 d3.select('html').selectAppend('div.tooltip').classed('tooltip-hidden', 1)
 
